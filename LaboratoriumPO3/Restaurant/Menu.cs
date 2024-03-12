3be7d5d81592a099dc8e6a26b1f0772b38124d52
h1msa <mikolaj.juchimowicz@microsoft.wsei.edu.pl>
2024-03-12 11:31:01 +0100

    lekki update

--- LaboratoriumPO3/Restaurant/Menu.cs
+++ LaboratoriumPO3/Restaurant/Menu.cs
@@ -5,4 +5,24 @@ public class Menu
     public List<MenuItem> Items { get; set; }
     public List<Order> Orders { get; set; }
     public DateOnly Published { get; init; }
+
+    public void PrintMenu()
+    {
+        
+    }
+
+    public void RemoveItem(int position)
+    {
+        
+    }
+
+    public void ChooseItem(int position)
+    {
+        
+    }
+
+    public void Price()
+    {
+        
+    }
 }
